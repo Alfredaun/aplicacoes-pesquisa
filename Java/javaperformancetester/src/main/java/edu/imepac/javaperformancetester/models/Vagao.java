37d41a1ee8eb5a3b940bbe740eba5c5d9e587d22
Alfredo <alfredo.oliveira@aluno.imepac.edu.br>
2025-05-09 18:56:13 -0300

    funciona

--- Java/javaperformancetester/src/main/java/edu/imepac/javaperformancetester/models/Vagao.java
+++ Java/javaperformancetester/src/main/java/edu/imepac/javaperformancetester/models/Vagao.java
@@ -1,6 +1,7 @@
 package edu.imepac.javaperformancetester.models;
 
 import edu.imepac.javaperformancetester.enums.Carga;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -8,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,7 +20,7 @@ public class Vagao {
     private Long id;
 
     private String placa;
-    private Carga carga;
+    private Carga carga;//soja,SPC,milho,ureia,acucar
     private Long tara;
     private Long pesoBruto;
     private Long pesoLiquido;

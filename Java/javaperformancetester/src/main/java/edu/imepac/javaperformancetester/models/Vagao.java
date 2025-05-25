698c5ebd60cc42053be9c648a62dda26686253d2
Alfredo <alfredo.oliveira@aluno.imepac.edu.br>
2025-05-25 19:37:25 -0300

    consertei o problema no encoste

--- Java/javaperformancetester/src/main/java/edu/imepac/javaperformancetester/models/Vagao.java
+++ Java/javaperformancetester/src/main/java/edu/imepac/javaperformancetester/models/Vagao.java
@@ -1,10 +1,7 @@
 package edu.imepac.javaperformancetester.models;
 
 import edu.imepac.javaperformancetester.enums.Carga;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,11 +14,21 @@ public class Vagao {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private Long id;
 
+    @Column(name = "placa")
     private String placa;
+
+    @Column(name = "carga")
     private Carga carga;//soja,SPC,milho,ureia,acucar
+
+    @Column(name = "tara")
     private Long tara;
+
+    @Column(name = "peso_bruto")
     private Long pesoBruto;
+
+    @Column(name = "ultima_carga")
     private Carga ultimaCarga;//soja,SPC,milho,ureia,acucar
 }
